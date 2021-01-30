c69ee9773623b0ca42ff78b3bdca99f6d89595d2
backendPanda <mensahje@gmail.com>
2021-01-30 11:20:45 +0000

    first

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,27 @@
+// rcc
+import React, { Component } from "react";
+import { RoomContext } from "../context";
+import { FaRProject } from "react-icons/fa";
+import Loading from "../Loading";
+import Room from "./Room";
+import Title from "./Title";
+export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
+  render() {
+    let { loading, featuredRooms: rooms } = this.context;
+
+    rooms = rooms.map((room) => {
+      return <Room key={room.id} room={room} />;
+    });
+
+    return (
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+        <Loading />
+      </section>
+    );
+  }
+}
